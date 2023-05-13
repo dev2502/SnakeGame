49446e3d921fa361cd1f41fff970ba0b9cfd552a
dev2502 <97377647+dev2502@users.noreply.github.com>
2023-05-14 02:47:39 +0530

    1st commit

---
+++ js/index.js
@@ -0,0 +1,146 @@
+//game constants & variables
+let inputDir = {x: 0, y: 0};
+const speed = 4;
+let lastPaintTime = 0;
+let snakeArr= [
+    //head of snake
+    {x:13, y:15}
+]
+let food = {x: 6, y: 7};
+let score = 0;
+let hiscoreval=0;
+const board = document.getElementById('board');
+const hiscoreBoard = document.getElementById('hiscoreBoard');
+
+//game functions
+function main(cTime){
+    window.requestAnimationFrame(main);
+    if( (cTime - lastPaintTime)/1000 < (1/speed) ){
+        return;
+    }
+    lastPaintTime = cTime ;
+    gameEngine();
+}
+
+function isCollide(snake) {
+    // If you bump into yourself 
+    for (let i = 1; i < snakeArr.length; i++) {            if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        }
+    }
+    // If you bump into the wall
+    if(snake[0].x >= 20 || snake[0].x <=0 || snake[0].y >= 20 || snake[0].y <=0){
+    return true;
+    }
+            
+    return false;
+}
+
+function gameEngine(){
+    //part1 -> updating the snake variable i.e. array
+    if(isCollide(snakeArr)){
+        inputDir = {x: 0 , y: 0};
+        alert("GAME OVER. Press any key to play again.");
+        snakeArr = [{x:13, y:15}];
+        score = 0;
+    }
+
+    // if snake has eaten the food incremnet the score and regenerate the food
+    if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
+        score += 1; //incrementing the score
+        if(score>hiscoreval){
+            hiscoreval = score;
+            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+            hiscoreBoard.innerHTML = "HIGH SCORE : " + hiscoreval;
+        }
+        scoreBoard.innerHTML = "Score : " + score ;
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x , y: snakeArr[0].y + inputDir.y});
+
+        let a=18;
+        let b=18;
+
+        food = { x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        
+    }
+
+
+    //moving the snake
+    for(let i=snakeArr.length - 2 ; i>=0 ; i--){
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
+
+    //part2 -> Display the snake
+    board.innerHTML = "";
+    snakeArr.forEach((e, index) => {
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+
+        if(index === 0)
+        {
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    });
+
+    //part2 -> Display the food
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement);
+}
+
+//main logic starts here
+
+let hiscore = localStorage.getItem("hiscore");
+if(hiscore === null){
+    hiscoreval = 0;
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+}
+else{
+    // hiscoreval = JSON.parse(hiscore);
+    hiscoreBoard.innerHTML = "HiScore: " + hiscore;
+}
+
+window.requestAnimationFrame(main);
+window.addEventListener('keydown', e=>{
+
+    //start the game
+    inputDir = {x: 0, y: 1};
+
+    switch (e.key) {
+        case "ArrowUp" :
+            console.log("ArrowUp");
+            inputDir.x = 0 ;
+            inputDir.y = -1 ;
+            break;
+
+        case "ArrowDown" :
+            console.log("ArrowDown");
+            inputDir.x = 0 ;
+            inputDir.y = 1 ;
+            break;
+
+        case "ArrowLeft" :
+            console.log("ArrowLeft");
+            inputDir.x = -1 ;
+            inputDir.y = 0 ;
+            break;
+
+        case "ArrowRight" :
+            console.log("ArrowRight");
+            inputDir.x = 1 ;
+            inputDir.y = 0 ;
+            break;
+
+        default  :
+            break;
+    }
+});
